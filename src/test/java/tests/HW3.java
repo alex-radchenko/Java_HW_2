d82b293b4e800c1e4d48a9a30edaf532104a0655
Alex-temp-tech <6504395845.ale.x@gmail.com>
2021-03-17 20:37:38 -0700

    HW3 - Configuration.remote = "http://localhost:4444/wd/hub";

--- src/test/java/tests/HW3.java
+++ src/test/java/tests/HW3.java
@@ -2,6 +2,8 @@ package tests;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,7 +12,9 @@ import org.junit.jupiter.api.Test;
 public class HW3 {
     @BeforeEach
     public void setUp() {
+//        Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = "chrome";
+
         Configuration.headless = false;
     }
     @Test
@@ -40,5 +44,35 @@ public class HW3 {
         $("#column-a").shouldHave(Condition.text("B"));
         $("#column-b").shouldHave(Condition.text("A"));
     }
+
+    @Test
+    void dragAndDropTestJS() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
+        executeJavaScript("function createEvent(typeOfEvent) {\n" + "var event =document.createEvent(\"CustomEvent\");\n"
+                + "event.initCustomEvent(typeOfEvent,true, true, null);\n" + "event.dataTransfer = {\n" + "data: {},\n"
+                + "setData: function (key, value) {\n" + "this.data[key] = value;\n" + "},\n"
+                + "getData: function (key) {\n" + "return this.data[key];\n" + "}\n" + "};\n" + "return event;\n"
+                + "}\n" + "\n" + "function dispatchEvent(element, event,transferData) {\n"
+                + "if (transferData !== undefined) {\n" + "event.dataTransfer = transferData;\n" + "}\n"
+                + "if (element.dispatchEvent) {\n" + "element.dispatchEvent(event);\n"
+                + "} else if (element.fireEvent) {\n" + "element.fireEvent(\"on\" + event.type, event);\n" + "}\n"
+                + "}\n" + "\n" + "function simulateHTML5DragAndDrop(element, destination) {\n"
+                + "var dragStartEvent =createEvent('dragstart');\n" + "dispatchEvent(element, dragStartEvent);\n"
+                + "var dropEvent = createEvent('drop');\n"
+                + "dispatchEvent(destination, dropEvent,dragStartEvent.dataTransfer);\n"
+                + "var dragEndEvent = createEvent('dragend');\n"
+                + "dispatchEvent(element, dragEndEvent,dropEvent.dataTransfer);\n" + "}\n" + "\n"
+                + "var source = arguments[0];\n" + "var destination = arguments[1];\n"
+                + "simulateHTML5DragAndDrop(source,destination);", $("#column-a"), $("#column-b"));
+
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        Selenide.sleep(3333);
+    }
+
 
 }
